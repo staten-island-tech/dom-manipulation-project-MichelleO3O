26dea84407078e4d2537048d051f799cdb125483
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2022-11-15 10:17:05 -0500

    DOM 6

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const DOMSelectors = {
   title: document.getElementById("title"),
   artist: document.getElementById("artist"),
   url: document.getElementById("url"),
+  button: document.getElementById("btn"),
+  display: document.getElementById("display"),
 };
+
+DOMSelectors.button.addEventListener();
