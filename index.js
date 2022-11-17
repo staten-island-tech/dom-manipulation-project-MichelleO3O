4b846fa0d490130038b48c6b3c76412f7c6569a6
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2022-11-17 10:15:18 -0500

    DOM 8

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const DOMSelectors = {
   title: document.getElementById("title"),
   artist: document.getElementById("artist"),
   url: document.getElementById("url"),
-  button: document.querySelector("btn"),
-  display: document.getElementById("display"),
+  submit: document.getElementById("form"),
 };
 
 DOMSelectors.submit.addEventListener("submit", function (e) {
   e.preventDefault();
   console.log(e);
 });
+
+// make sure to create seperate functions!!//
